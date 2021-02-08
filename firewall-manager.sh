a7fcd9312c9839a9dc75b04af2ccdc30755d5d12
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-08 13:56:39 -0500

    Update firewall-manager.sh

--- firewall-manager.sh
+++ firewall-manager.sh
@@ -34,24 +34,24 @@ FIRWALL_MANAGER="$FIRWALL_MANAGER_PATH/firewall-manager"
 # Install the firewall
 function install-firewall() {
   if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ] || [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ] || [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ] || [ "$DISTRO" == "alpine" ] || [ "$DISTRO" == "freebsd" ]; }; then
-  if { [ ! -x "$(command -v sed)" ] || [ ! -x "$(command -v curl)" ] || [ ! -x "$(command -v jq)" ] || [ ! -x "$(command -v ufw)" ] || [ ! -x "$(command -v fail2ban)" ] || [ ! -x "$(command -v ssh)" ] || [ ! -x "$(command -v openssl)" ] || [ ! -x "$(command -v lsof)" ]; }; then
-    if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ]; }; then
-      apt-get update
-      apt-get install haveged fail2ban ufw lsof openssh-client openssh-server openssl jq curl sed lsof -y
-    elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
-      yum update -y
-      yum install haveged fail2ban ufw lsof openssh-client openssh-server openssl jq curl sed lsof -y
-    elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
-      pacman -Syu
-      pacman -Syu --noconfirm haveged fail2ban ufw lsof openssh-client openssh-server openssl jq curl sed lsof
-    elif [ "$DISTRO" == "alpine" ]; then
-      apk update
-      apk add haveged fail2ban ufw lsof openssh-client openssh-server openssl jq curl sed lsof
-    elif [ "$DISTRO" == "freebsd" ]; then
-      pkg update
-      pkg install haveged fail2ban ufw lsof openssh-client openssh-server openssl jq curl sed lsof
+    if { [ ! -x "$(command -v sed)" ] || [ ! -x "$(command -v curl)" ] || [ ! -x "$(command -v jq)" ] || [ ! -x "$(command -v ufw)" ] || [ ! -x "$(command -v fail2ban)" ] || [ ! -x "$(command -v ssh)" ] || [ ! -x "$(command -v openssl)" ] || [ ! -x "$(command -v lsof)" ]; }; then
+      if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ]; }; then
+        apt-get update
+        apt-get install haveged fail2ban ufw lsof openssh-client openssh-server openssl jq curl sed lsof -y
+      elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
+        yum update -y
+        yum install haveged fail2ban ufw lsof openssh-client openssh-server openssl jq curl sed lsof -y
+      elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
+        pacman -Syu
+        pacman -Syu --noconfirm haveged fail2ban ufw lsof openssh-client openssh-server openssl jq curl sed lsof
+      elif [ "$DISTRO" == "alpine" ]; then
+        apk update
+        apk add haveged fail2ban ufw lsof openssh-client openssh-server openssl jq curl sed lsof
+      elif [ "$DISTRO" == "freebsd" ]; then
+        pkg update
+        pkg install haveged fail2ban ufw lsof openssh-client openssh-server openssl jq curl sed lsof
+      fi
     fi
-  fi
   else
     echo "Error: $DISTRO not supported."
     exit
@@ -125,9 +125,9 @@ create-user
 function firwall-manager() {
   if [ ! -d "$FIRWALL_MANAGER_PATH" ]; then
     mkdir -p $FIRWALL_MANAGER_PATH
-      if [ ! -f "$FIRWALL_MANAGER" ]; then
-        echo "Firewall Manager: True" >> $FIRWALL_MANAGER
-      fi
+    if [ ! -f "$FIRWALL_MANAGER" ]; then
+      echo "Firewall Manager: True" >>$FIRWALL_MANAGER
+    fi
   fi
 }
 
@@ -167,13 +167,12 @@ function enable-service() {
 
 enable-service
 
-
-function ufw-rules() 
-  if [ -x "$(command -v fail2ban)" ]; then
-  if [ "$(lsof -i :80)" ]; then
-    ufw allow 80/tcp
-  elif [ "$(lsof -i :443)" ]; then
-    ufw allow 443/tcp
-  fi
+function ufw-rules() {
+  if [ -x "$(command -v ufw)" ]; then
+    if [ "$(lsof -i :80)" ]; then
+      ufw allow 80/tcp
+    elif [ "$(lsof -i :443)" ]; then
+      ufw allow 443/tcp
+    fi
   fi
 }
