96bb81a6edf3ae8d0cc6a10739cf5a63f07b2431
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-08 09:40:10 -0500

    Update firewall-manager.sh

--- firewall-manager.sh
+++ firewall-manager.sh
@@ -134,7 +134,17 @@ function create-user() {
     USERNAME="$(openssl rand -hex 10)"
     PASSWORD="$(openssl rand -base64 50)"
     useradd -m -s /bin/bash "$USERNAME" -p "$PASSWORD"
+    SERVER_HOST="$(curl -4 -s 'https://api.ipengine.dev' | jq -r '.network.ip')"
+    eval "$(ssh-agent -s)"
+    ssh-keygen -o -a 10000 -t ed25519 -f /home/"$USERNAME"/.ssh/id_ed25519 -C "$USERNAME@$SERVER_HOST"
+    ssh-add /home/"$USERNAME"/.ssh/id_ed25519
+    SSH_PRIVATE_KEY="$(cat /home/"$USERNAME"/.ssh/id_ed25519)"
+    SSH_PUBLIC_KEY="$(cat /home/"$USERNAME"/.ssh/id_ed25519.pub)"
+    rm -f /home/"$USERNAME"/.ssh/authorized_keys
+    echo "$SSH_PUBLIC_KEY" >>/home/"$USERNAME"/.ssh/authorized_keys
+    chmod 600 /home/"$USERNAME"/.ssh/authorized_keys
     echo "Username: $USERNAME"
+    echo "SSH Key: $SSH_PRIVATE_KEY"
     echo "Password: $PASSWORD"
     echo "Root login has been disabled"
   fi
@@ -147,3 +157,5 @@ function firwall-manager() {
     echo "Firewall Manager: True" >> $FIRWALL_MANAGER
   fi
 }
+
+firwall-manager
