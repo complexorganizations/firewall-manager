fc53c863bdc97a5a50943b1bcfa31362dc683378
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-24 20:17:15 -0500

    Update firewall-manager.sh

--- firewall-manager.sh
+++ firewall-manager.sh
@@ -102,22 +102,14 @@ configure-firewall
 
 function create-user() {
   if [ ! -f "$FIRWALL_MANAGER" ]; then
-    USERNAME="$(openssl rand -hex 5)"
-    PASSWORD="$(openssl rand -base64 25)"
-    useradd -m -s /bin/bash "$USERNAME" -p "$PASSWORD"
-    usermod -aG sudo "$USERNAME"
-    SERVER_HOST="$(curl -4 -s 'https://api.ipengine.dev' | jq -r '.network.ip')"
-    eval "$(ssh-agent -s)"
-    ssh-keygen -o -a 10000 -t ed25519 -f /home/"$USERNAME"/.ssh/id_ed25519 -C "$USERNAME@$SERVER_HOST"
-    ssh-add /home/"$USERNAME"/.ssh/id_ed25519
-    SSH_PRIVATE_KEY="$(cat /home/"$USERNAME"/.ssh/id_ed25519)"
-    SSH_PUBLIC_KEY="$(cat /home/"$USERNAME"/.ssh/id_ed25519.pub)"
-    rm -f /home/"$USERNAME"/.ssh/authorized_keys
-    echo "$SSH_PUBLIC_KEY" >>/home/"$USERNAME"/.ssh/authorized_keys
-    chmod 600 /home/"$USERNAME"/.ssh/authorized_keys
-    echo "Username: $USERNAME"
-    echo "SSH Key: $SSH_PRIVATE_KEY"
-    echo "Password: $PASSWORD"
+    LINUX_USERNAME="$(openssl rand -hex 5)"
+    LINUX_PASSWORD="$(openssl rand -hex 10)"
+    useradd -m -s /bin/bash "$LINUX_USERNAME"
+    echo -e "$LINUX_PASSWORD\n$LINUX_PASSWORD" | passwd "$LINUX_USERNAME"
+    usermod -aG sudo "$LINUX_USERNAME"
+    echo "Linux Information"
+    echo "Username: $LINUX_USERNAME"
+    echo "Password: $LINUX_PASSWORD"
   fi
 }
 
