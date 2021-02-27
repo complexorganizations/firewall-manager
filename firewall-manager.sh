297760703e45b6aa6965e96107af8818d3b54388
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-26 22:43:19 -0500

    Update firewall-manager.sh

--- firewall-manager.sh
+++ firewall-manager.sh
@@ -3,7 +3,7 @@
 
 # Require script to be run as root
 function super-user-check() {
-  if [ "$EUID" -ne 0 ]; then
+  if [ "${EUID}" -ne 0 ]; then
     echo "You need to run this script as super user."
     exit
   fi
@@ -17,7 +17,7 @@ function dist-check() {
   if [ -e /etc/os-release ]; then
     # shellcheck disable=SC1091
     source /etc/os-release
-    DISTRO=$ID
+    DISTRO=${ID}
   fi
 }
 
@@ -28,31 +28,31 @@ UFW_CONFIG="/etc/default/ufw"
 SSHD_CONFIG="/etc/ssh/sshd_config"
 NGINX_CONFIG="/etc/nginx/nginx.conf"
 FIRWALL_MANAGER_PATH="/etc/firewall-manager"
-FIRWALL_MANAGER="$FIRWALL_MANAGER_PATH/firewall-manager"
+FIRWALL_MANAGER="${FIRWALL_MANAGER_PATH}/firewall-manager"
 
 # Install the firewall
 function install-firewall() {
-  if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ] || [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ] || [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ] || [ "$DISTRO" == "alpine" ] || [ "$DISTRO" == "freebsd" ]; }; then
+  if { [ "${DISTRO}" == "ubuntu" ] || [ "${DISTRO}" == "debian" ] || [ "${DISTRO}" == "raspbian" ] || [ "${DISTRO}" == "pop" ] || [ "${DISTRO}" == "kali" ] || [ "${DISTRO}" == "linuxmint" ] || [ "${DISTRO}" == "fedora" ] || [ "${DISTRO}" == "centos" ] || [ "${DISTRO}" == "rhel" ] || [ "${DISTRO}" == "arch" ] || [ "${DISTRO}" == "manjaro" ] || [ "${DISTRO}" == "alpine" ] || [ "${DISTRO}" == "freebsd" ]; }; then
     if { [ ! -x "$(command -v sed)" ] || [ ! -x "$(command -v curl)" ] || [ ! -x "$(command -v jq)" ] || [ ! -x "$(command -v ufw)" ] || [ ! -x "$(command -v fail2ban)" ] || [ ! -x "$(command -v ssh)" ] || [ ! -x "$(command -v openssl)" ] || [ ! -x "$(command -v lsof)" ]; }; then
-      if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ]; }; then
+      if { [ "${DISTRO}" == "ubuntu" ] || [ "${DISTRO}" == "debian" ] || [ "${DISTRO}" == "raspbian" ] || [ "${DISTRO}" == "pop" ] || [ "${DISTRO}" == "kali" ] || [ "${DISTRO}" == "linuxmint" ]; }; then
         apt-get update
         apt-get install haveged fail2ban ufw lsof openssh-server openssl jq curl sed lsof -y
-      elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
+      elif { [ "${DISTRO}" == "fedora" ] || [ "${DISTRO}" == "centos" ] || [ "${DISTRO}" == "rhel" ]; }; then
         yum update -y
         yum install haveged fail2ban ufw lsof openssh-server openssl jq curl sed lsof -y
-      elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
+      elif { [ "${DISTRO}" == "arch" ] || [ "${DISTRO}" == "manjaro" ]; }; then
         pacman -Syu
         pacman -Syu --noconfirm haveged fail2ban ufw lsof openssh-server openssl jq curl sed lsof
-      elif [ "$DISTRO" == "alpine" ]; then
+      elif [ "${DISTRO}" == "alpine" ]; then
         apk update
         apk add haveged fail2ban ufw lsof openssh-server openssl jq curl sed lsof
-      elif [ "$DISTRO" == "freebsd" ]; then
+      elif [ "${DISTRO}" == "freebsd" ]; then
         pkg update
         pkg install haveged fail2ban ufw lsof openssh-server openssl jq curl sed lsof
       fi
     fi
   else
-    echo "Error: $DISTRO not supported."
+    echo "Error: ${DISTRO} not supported."
     exit
   fi
 }
@@ -63,30 +63,30 @@ install-firewall
 function configure-firewall() {
   # SSH
   if [ -x "$(command -v sshd)" ]; then
-    if [ -f "$SSHD_CONFIG" ]; then
-      sed -i "s|#PasswordAuthentication yes|PasswordAuthentication no|" $SSHD_CONFIG
-      sed -i "s|#PermitEmptyPasswords no|PermitEmptyPasswords no|" $SSHD_CONFIG
-      sed -i "s|AllowTcpForwarding yes|AllowTcpForwarding no|" $SSHD_CONFIG
-      sed -i "s|PermitRootLogin yes|PermitRootLogin no|" $SSHD_CONFIG
-      sed -i "s|#MaxAuthTries 6|MaxAuthTries 3|" $SSHD_CONFIG
-      sed -i "s|X11Forwarding yes|X11Forwarding no|" $SSHD_CONFIG
-      sed -i "s|#LogLevel INFO|LogLevel VERBOSE|" $SSHD_CONFIG
-      sed -i "s|#Port 22|Port 22|" $SSHD_CONFIG
-      sed -i "s|#PubkeyAuthentication yes|PubkeyAuthentication yes|" $SSHD_CONFIG
-      sed -i "s|#ChallengeResponseAuthentication no|ChallengeResponseAuthentication yes|" $SSHD_CONFIG
+    if [ -f "${SSHD_CONFIG}" ]; then
+      sed -i "s|#PasswordAuthentication yes|PasswordAuthentication no|" ${SSHD_CONFIG}
+      sed -i "s|#PermitEmptyPasswords no|PermitEmptyPasswords no|" ${SSHD_CONFIG}
+      sed -i "s|AllowTcpForwarding yes|AllowTcpForwarding no|" ${SSHD_CONFIG}
+      sed -i "s|PermitRootLogin yes|PermitRootLogin no|" ${SSHD_CONFIG}
+      sed -i "s|#MaxAuthTries 6|MaxAuthTries 3|" ${SSHD_CONFIG}
+      sed -i "s|X11Forwarding yes|X11Forwarding no|" ${SSHD_CONFIG}
+      sed -i "s|#LogLevel INFO|LogLevel VERBOSE|" ${SSHD_CONFIG}
+      sed -i "s|#Port 22|Port 22|" ${SSHD_CONFIG}
+      sed -i "s|#PubkeyAuthentication yes|PubkeyAuthentication yes|" ${SSHD_CONFIG}
+      sed -i "s|#ChallengeResponseAuthentication no|ChallengeResponseAuthentication yes|" ${SSHD_CONFIG}
     fi
   fi
   # UFW
   if [ -x "$(command -v ufw)" ]; then
-    sed -i "s|# IPV6=yes;|IPV6=yes;|" $UFW_CONFIG
+    sed -i "s|# IPV6=yes;|IPV6=yes;|" ${UFW_CONFIG}
     ufw default allow incoming
     ufw default allow outgoing
     ufw allow 22/tcp
   fi
   # Nginx
   if [ -x "$(command -v nginx)" ]; then
-    if [ -f "$NGINX_CONFIG" ]; then
-      sed -i "s|# server_tokens off|server_tokens off|" $NGINX_CONFIG
+    if [ -f "${NGINX_CONFIG}" ]; then
+      sed -i "s|# server_tokens off|server_tokens off|" ${NGINX_CONFIG}
     fi
   fi
 }
@@ -94,25 +94,25 @@ function configure-firewall() {
 configure-firewall
 
 function create-user() {
-  if [ ! -f "$FIRWALL_MANAGER" ]; then
+  if [ ! -f "${FIRWALL_MANAGER}" ]; then
     LINUX_USERNAME="$(openssl rand -hex 5)"
     LINUX_PASSWORD="$(openssl rand -hex 10)"
-    useradd -m -s /bin/bash "$LINUX_USERNAME"
-    echo -e "$LINUX_PASSWORD\n$LINUX_PASSWORD" | passwd "$LINUX_USERNAME"
-    usermod -aG sudo "$LINUX_USERNAME"
+    useradd -m -s /bin/bash "${LINUX_USERNAME}"
+    echo -e "${LINUX_PASSWORD}\n${LINUX_PASSWORD}" | passwd "${LINUX_USERNAME}"
+    usermod -aG sudo "${LINUX_USERNAME}"
     echo "Linux Information"
-    echo "Username: $LINUX_USERNAME"
-    echo "Password: $LINUX_PASSWORD"
+    echo "Username: ${LINUX_USERNAME}"
+    echo "Password: ${LINUX_PASSWORD}"
   fi
 }
 
 create-user
 
 function firwall-manager() {
-  if [ ! -d "$FIRWALL_MANAGER_PATH" ]; then
-    mkdir -p $FIRWALL_MANAGER_PATH
-    if [ ! -f "$FIRWALL_MANAGER" ]; then
-      echo "Firewall Manager: True" >>$FIRWALL_MANAGER
+  if [ ! -d "${FIRWALL_MANAGER_PATH}" ]; then
+    mkdir -p ${FIRWALL_MANAGER_PATH}
+    if [ ! -f "${FIRWALL_MANAGER}" ]; then
+      echo "Firewall Manager: True" >>${FIRWALL_MANAGER}
     fi
   fi
 }
